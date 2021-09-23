d76b3f4b2d53a8314bc0be9673754c0e6cf69c53
Vikram Negi <viknegi0@gmail.com>
2021-09-23 14:30:01 +0530

    smtp test  working

--- mailer.js
+++ mailer.js
@@ -1,4 +1,35 @@
+const nodemailer = require("nodemailer");
+const { prependListener } = require("process");
 require("dotenv").config();
 
-console.log(process.env.EMAIL)
-console.log(process.env.PASS)
+const email = process.env.EMAIL;
+const pass = process.env.PASS;
+
+async function main() {
+  // dummy test mail account
+  let testAcc = await nodemailer.createTestAccount();
+
+  // transporter obj using default SMTP
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: `${email}`,
+      pass: `${pass}`,
+    },
+  });
+
+  // send mail
+  let info = await transporter.sendMail({
+    from: `Vikram Negi <${email}>`,
+    to: `Vikram Negi <${pass}>`,
+    subject: "Send using node.js",
+    text: "hello, world",
+    html: "<h3 style=\"font-weight: 300\">Hello from the other side!</h3>",
+  });
+
+  console.log("Messeage sent: %s", info.messageId);
+}
+
+main().catch(err => console.log(err));
