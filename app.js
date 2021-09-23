ef6dc8184e63f75708dcea05d89bb6e7b83e2929
Vikram Negi <viknegi0@gmail.com>
2021-09-24 00:04:21 +0530

    express boi

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// const express = require("express");
+const express = require("express");
 const sgMail = require("@sendgrid/mail");
 const mailOptions = require("./js/testMail");
 require("dotenv").config();
 
-// const app = express();
-// const port = 3000;
+const app = express();
+const port = 3000;
 
-// app.listen(port, () => {
-//   console.log(`Listening at http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
 
 // Using Twillio SendGrid's API
 sgMail.setApiKey(process.env.API_KEY);
