7bc7e04242f24f7b900b5b6d59e1702705079ff9
Vikram Negi <viknegi0@gmail.com>
2021-09-23 21:39:32 +0530

    535 status code

--- js/testMail.js
+++ js/testMail.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 // If there are errors, please check this object.
 module.exports = {
-  from: `Vikram Negi <${process.env.EMAIL_ID}>`,
-  to: `Vikram Negi <${process.env.EMAIL_ID}>`,
-  subject: "Send using node.js",
-  html: "<h3 style=\"font-weight: 300\">Hello, World!</h3>",
+  from: `${process.env.EMAIL}`,
+  to: `${process.env.EMAIL}, yogeshnegi987@gmail.com`,
+  subject: "Please work!",
+  text: "If you're able to view this message, then I have successfully sent an email using Node.js"
 };
--- mailer.js
+++ mailer.js
@@ -1,24 +1,52 @@
 const nodemailer = require("nodemailer");
+const express = require("express");
 require("dotenv").config();
 const mailOptions = require("./js/testMail");
+const {google} = require("googleapis");
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.OAUTH_CLIENT_ID,
+  process.env.OAUTH_CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
+
+oauth2Client.setCredentials({
+  refresh_token: process.env.OAUTH_REFRESH_TOKEN
+});
+
+const accessToken = oauth2Client.getAccessToken();
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
 
 async function main() {
-
+  // transporter, with all the tokens required
   let transporter = nodemailer.createTransport({
+    tls: {
+      rejectUnauthorized: false
+    },
     service: "gmail",
     auth: {
       type: "OAuth2",
-      user: process.env.EMAIL_ID,
-      clientId: process.env.OAUTH_CLIENTID,
+      user: process.env.EMAIL,
+      pass: process.env.APP_PASS,
+      clientId: process.env.OAUTH_CLIENT_ID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
       refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-      accessToken: process.env.OAUTH_ACCESS_TOKEN
+      accessToken: accessToken
     },
   });
 
-  // send mail
+  // send mail {mailOptions can be subject to change}
   let info = await transporter.sendMail(mailOptions);
 
-  console.log("Messeage sent: %s", info.messageId);
+  console.log(`Messeage sent: ${info}`);
+
+  transporter.close();
 }
 
 main().catch(err => console.log(err));
